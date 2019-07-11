33940b6884e6741ed50ccd612c2271a2f641cfe4
harry <harry@DESKTOP-6NUR148>
2019-07-11 21:14:39 +1200

    javadoc add and error fixed where addproduct quantity waas being multiplied rather than being added

--- JavaBoxApp/src/packer/Box.java
+++ JavaBoxApp/src/packer/Box.java
@@ -2,7 +2,8 @@ package packer;
 
 /**
  *
- * @author I.M.Bad
+ * @author Harry Furnish
+ * @version 1.0
  */
 public class Box {
     
@@ -10,28 +11,42 @@ public class Box {
     private Manifest contents;
     private Customer customer;
     private Depot depot;
-    private double Capacity = 20;
-
+    private double Capacity = 21;
+    /**
+     * 
+     * @param customer customer details for box label
+     * @param depot depot details for box label
+     */
     public Box(Customer customer, Depot depot) {
         this.customer = customer;
         this.depot = depot;
         contents = new Manifest();
     }
-    
+    /**
+     * 
+     * @param product product to be added into box
+     */
     public void addProduct(Product product) {
         if (canFit(product)) {
             contents.addProduct(product, 1);
         }
     }
     
-    
+    /**
+     * 
+     * @param product product to be added into box
+     * @param quantity quantity of product to be added into box
+     */
     
     public void addProduct(Product product, int quantity) {
         if (canFit(product,quantity)); {
             contents.addProduct(product, quantity);
         }
     }
-   
+   /**
+    * 
+    * @return string containing customer/depot details and box contents warning label details
+    */
     public String getLabel() {
         StringBuilder label = new StringBuilder();
         label.append(customer);
@@ -52,11 +67,17 @@ public class Box {
         
         return label.toString();
     }
-    
+    /**
+     * 
+     * @return string label details
+     */
     public String toString() {
         return getLabel();
     }
-    
+    /**
+     * 
+     * @return double total weight of box contents
+     */
     public double getTotalWeight() {
         return contents.getTotalWeight();
     }
@@ -67,25 +88,48 @@ public class Box {
         }
     }
     */
+    /**
+     * 
+     * @param p product to be tested to see if will fit in box
+     * @return boolean can or cannot fit in box
+     */
     public boolean canFit(Product p) {
         return p.getWeight() < Capacity - this.getTotalWeight();
     }
-    
+    /**
+     * 
+     * @param p product to be tested to see if will fit in box
+     * @param quantity integer number of product items to be fitted in box
+     * @return boolean can or cannot fit all products in box
+     */
     public boolean canFit(Product p, int quantity) {
         return (p.getWeight() * quantity) < Capacity - this.getTotalWeight();
     }
-    
+    /**
+     * 
+     * @return double amount of weight capacity remaining in box
+     */
     public double remainingCapacity() {
         return Capacity - this.getTotalWeight();
     }
-    
+    /**
+     * 
+     * @return boolean contains or does not contain fragile product 
+     */
     public boolean isFragile() {
         return contents.hasFragileItems();
     }
-    
+    /**
+     * 
+     * @return boolean contains or does not contain hazardous product
+     */
     public boolean isHazardous() {
         return contents.hasHazardousItems();
     }
+    /**
+     * 
+     * @return boolean box is considered or not consider heavy
+     */
     public boolean isHeavy(){
         return this.getTotalWeight() >= 15;
     }
